70a428b153b7ae1e365d7732bae1f5a21d6b92ee
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2025-12-28 12:11:08 +0600

    register section updated

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -47,7 +47,7 @@ const Register = () => {
             <input name="password" type="password" placeholder="Min 6 characters" className="input input-bordered w-full bg-[#2a323c] focus:border-[#00d1b2]" required />
           </div>
 
-          <button type="submit" className="btn bg-[#00d1b2] hover:bg-[#00b89d] border-none text-black font-bold w-full mt-4 rounded-full text-lg shadow-lg transition-transform hover:scale-105">
+          <button type="submit" className="btn bg-[#00d1b2] hover:bg-[#00b89d] border-none rounded-xl text-black font-bold w-full mt-4 rounded-full text-lg shadow-lg transition-transform hover:scale-105">
             Register Now
           </button>
         </form>
@@ -61,11 +61,11 @@ const Register = () => {
           className="btn btn-outline border-gray-600 text-white w-full rounded-full flex items-center justify-center gap-3 hover:bg-white hover:text-black transition-all duration-300"
         >
           <img 
-            className="w-5 h-5" 
+            className="w-5 h-5 rounded-xl" 
             src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png" 
             alt="Google Logo" 
           />
-          <span className="font-semibold">Continue with Google</span>
+          <span className="font-semibold ">Continue with Google</span>
         </button>
 
         <p className="text-center mt-6 text-gray-400">
